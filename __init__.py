58c282915e52179bbeb065c1b396ed135746817d
David Dunn <qenopster@gmail.com>
2021-01-22 15:29:57 -0800

    separated backends to separate classes

--- __init__.py
+++ __init__.py
@@ -7,12 +7,12 @@ import numpy as np
 import cv2
 import time
 
-BACKEND = {'cv2':0, 'ptGrey':1, 'picamera':2} # different camera APIs that are supported
+BACKEND = {'cv2':0, 'flyCap':1, 'picamera':2} # different camera APIs that are supported
 try:
     from shared_modules.pyfly2 import pyfly2
 except ImportError:
-    print("Warning: ptGrey backend is not available.")
-    BACKEND['ptGrey'] = None
+    print("Warning: FlyCapture backend is not available.")
+    BACKEND['flyCap'] = None
 try:
     from picamera import PiCamera
     from picamera.array import PiRGBArray
@@ -31,18 +31,17 @@ def cv2CloseWindow(window):
 
 class Camera(object):
     ''' camera objects should store resolution and calibration data as well 
-        as provide methods for capturing and viewing content regardless of source of camera data (cv2 or ptGrey)
+        as provide methods for capturing and viewing content regardless of source of camera data
+        By default uses cv2 backend - use subclasses for other backends
     '''
-    def __init__(self,id=0,backend=BACKEND['cv2'],**kwargs):
+    def __init__(self,id=0,**kwargs):
         self.id = id
-        self.backend = backend
         self._cam = None
         self._cap = None
         self._context = None
-        self.resolution = (0,0)
+        self.resolution = (0,0) # some cameras need to be open to get resolution
         self.resolution = self.getResolution(locals())
-        self.np_resolution = self.getNPResolution()
-        self.fps = kwargs.get('fps', 15 if self.backend == BACKEND['ptGrey'] else 30)
+        self.fps = kwargs.get('fps', 30)
         self.close()
         self.shutter_speed = kwargs.get('shutter_speed',0)
         self.iso = kwargs.get('iso',0)
@@ -59,77 +58,23 @@ class Camera(object):
         if self.id is None:
             return 0
         if self._cap is None:
-            if self.backend == BACKEND['cv2']:
-                self._cap = cv2.VideoCapture(self.id)
-                if not self._cap.isOpened():          # check if we succeeded
-                    self._cap = None
-                    return 0
-                return 1
-            elif self.backend == BACKEND['ptGrey']:
-                self._context = pyfly2.Context()
-                if self._context.num_cameras < 1:
-                    #raise ValueError('No PointGrey cameras found')
-                    self._cap = None
-                    return 0
-                self._cap = self._context.get_camera(self.id)
-                self._cap.Connect()
-                self._cap.StartCapture()
-                print("Connected PointGrey camera %s" % self.id)
-                return 1
-            elif self.backend == BACKEND['picamera']:
-                self._cam = PiCamera()
-                self._cam.resolution = self.resolution
-                self._cam.framerate = self.fps
-                self._cap = PiRGBArray(self._cam, size=tuple(self._cam.resolution))
-                time.sleep(2)   # pause while the camera inits and settles
-                if self.shutter_speed is None:
-                    self.shutter_speed = self._cam.exposure_speed
-                self._cam.shutter_speed = self.shutter_speed
-                self._cam.iso = self.iso
-                self._cam.exposure_mode = self.exposure_mode
-                if self.awb_mode == 'off':
-                    g = self._cam.awb_gains
-                    self._cam.awb_mode = self.awb_mode
-                    self._cam.awb_gains = g
-                self._cam.awb_mode = self.awb_mode
-                return 1
+            self._cap = cv2.VideoCapture(self.id)
+            if not self._cap.isOpened():          # check if we succeeded
+                self._cap = None
+                return 0
+            return 1
         return 2
     def close(self):
-        if self._cap is not None:
-            if self.backend == BACKEND['cv2']:
-                self._cap.release()
-            elif self.backend == BACKEND['ptGrey']:
-                self._cap.StopCapture()
-            elif self.backend == BACKEND['picamera']:
-                self._continuousCapture = None
-                self._cap.close()
-                self._cam.close()
-                self._cam = None
+        if self._cap is not None:           
+            self._cap.release()
             self._cap = None
     release = close
     def read(self, video=False):
         if self.open():
-            if self.backend == BACKEND['cv2']:
-                ret, img = self._cap.read()
-                if ret != 1:
-                    raise IOError('Camera image not captured')
-                return img
-            elif self.backend == BACKEND['ptGrey']:
-                return self._cap.GrabNumPyImage('bgr')
-            elif self.backend == BACKEND['picamera']:
-                if not video:
-                    self._continuousCapture = None
-                    self._cam.capture(self._cap, format='bgr')
-                    image = self._cap.array
-                    self._cap.truncate(0)
-                    return image
-                else:
-                    if self._continuousCapture is None:
-                        self._continuousCapture = self._cam.capture_continuous(self._cap, format='bgr', use_video_port=True)
-                    frame = next(self._continuousCapture)
-                    image = frame.array
-                    self._cap.truncate(0)
-                    return image
+            ret, img = self._cap.read()
+            if ret != 1:
+                raise IOError('Camera image not captured')
+            return img     
     def readUndistort(self, video=False):
         image = self.read(video)
         if self.mapX is None or self.mapY is None:
@@ -144,16 +89,11 @@ class Camera(object):
     def captureUndistort(self):
         return captureFrames(self, True)
     def getResolution(self, kwargs=None):
-        if self.backend == BACKEND['picamera']:
-            return kwargs.get('resolution',kwargs.get('res',(640,480)))
-        elif self.open():
-            if self.backend == BACKEND['cv2']:
-                try:
-                    return (int(self._cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)), int(self._cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
-                except:
-                    return (int(self._cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-            elif self.backend == BACKEND['ptGrey']:
-                return self._cap.GetSize()
+        if self.open():
+            try:
+                return (int(self._cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)), int(self._cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+            except:
+                return (int(self._cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     def getNPResolution(self, kwargs=None):
         return (self.resolution[1], self.resolution[0])
     def calibrate(self, gridCorners, gridScale, **kwargs):     # flags=cv2.CALIB_FIX_K3
@@ -186,6 +126,92 @@ class Camera(object):
         self.matrix = npzfile['matrix']
         self.distortion = npzfile['distortion']
         self.error = npzfile['error']
+
+class FlyCamera(Camera):
+    def __init__(self,id=None,**kwargs):
+        if BACKEND['flyCap'] is None:
+            raise ImportError('dCamera Error: [pyFly2] interface not loaded!')
+        super().__init__(id,**kwargs)
+        self.fps = kwargs.get('fps', 15)
+    def open(self):
+        if self.id is None:
+            return 0
+        if self._cap is None:
+            self._context = pyfly2.Context()
+            if self._context.num_cameras < 1:
+                #raise ValueError('No PointGrey cameras found')
+                self._cap = None
+                return 0
+            self._cap = self._context.get_camera(self.id)
+            self._cap.Connect()
+            self._cap.StartCapture()
+            print("Connected PointGrey camera %s" % self.id)
+            return 1
+        return 2
+    def close(self):
+        if self._cap is not None:
+            self._cap.StopCapture()
+        self._cap = None
+    release = close
+    def read(self, video=False):
+        if self.open():
+            return self._cap.GrabNumPyImage('bgr')
+    def getResolution(self, kwargs=None):
+        if self.open():
+            return self._cap.GetSize()
+
+class PiCamera(Camera):
+    def __init__(self,id=None,**kwargs):
+        if BACKEND['picamera'] is None:
+            raise ImportError('dCamera Error: [picamera] interface not loaded!')
+        super().__init__(id,**kwargs)
+        ## TODO get the serial number and put it in self.id
+    def open(self):
+        if self.id is None:
+            return 0
+        if self._cap is None:
+            self._cam = PiCamera()
+            self._cam.resolution = self.resolution
+            self._cam.framerate = self.fps
+            self._cap = PiRGBArray(self._cam, size=tuple(self._cam.resolution))
+            time.sleep(2)   # pause while the camera inits and settles
+            if self.shutter_speed is None:
+                self.shutter_speed = self._cam.exposure_speed
+            self._cam.shutter_speed = self.shutter_speed
+            self._cam.iso = self.iso
+            self._cam.exposure_mode = self.exposure_mode
+            if self.awb_mode == 'off':
+                g = self._cam.awb_gains
+                self._cam.awb_mode = self.awb_mode
+                self._cam.awb_gains = g
+            self._cam.awb_mode = self.awb_mode
+            return 1
+        return 2
+    def close(self):
+        if self._cap is not None:
+            self._continuousCapture = None
+            self._cap.close()
+            self._cam.close()
+            self._cam = None
+        self._cap = None
+    def read(self, video=False):
+        if self.open():
+            if not video:
+                self._continuousCapture = None
+                self._cam.capture(self._cap, format='bgr')
+                image = self._cap.array
+                self._cap.truncate(0)
+                return image
+            else:
+                if self._continuousCapture is None:
+                    self._continuousCapture = self._cam.capture_continuous(self._cap, format='bgr', use_video_port=True)
+                frame = next(self._continuousCapture)
+                image = frame.array
+                self._cap.truncate(0)
+                return image
+    def getResolution(self, kwargs=None): # No way to query on a picamera
+        return kwargs.get('resolution',kwargs.get('res',(640,480)))
+
 
 def toGray(image):
     iy, ix, channels = image.shape if len(image.shape)>2 else [image.shape[0], image.shape[1], 1]
